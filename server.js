fb196184cfe29f37138f55f419be3f55824d3ba0
Tom <tomtomdinh@gmail.com>
2018-05-30 14:29:51 -0700

    add project page

--- server.js
+++ server.js
@@ -61,6 +61,12 @@ app.get('/about',(req,res)=>{
   });
 });
 
+app.get('/projects', (req,res)=> {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/bad',(req,res) => {
   res.send({
     errorMessage: 'u bad'
